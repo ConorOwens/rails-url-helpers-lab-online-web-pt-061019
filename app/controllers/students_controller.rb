bd0d77cb271e61a7d70a8f55c27a483f1a6a07c0
Conor Owens <conorj.owens@gmail.com>
2019-10-22 01:11:24 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,6 @@ class StudentsController < ApplicationController
   end
   
   def activate
-    @student = Student.find(params[:id])
     @student.active = !@student.active 
     @student.save
     redirect to student_path(@student)
